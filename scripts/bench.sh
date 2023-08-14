6cb8f16e3a803db80a501a789a3ea83ee03eca76
Shrinu Sivakumar <shrinu@openpop.com>
2023-08-13 19:33:22 -0700

    Adding bench script (which doesn't benchmark anything)
    Also some cleanup of Rust code

---
+++ scripts/bench.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# As a fairly janky test, this runs curl sequentially and in parallel 5 times respectively.
+# If you see COMPLETED! 5 times, then each call successfully returned.
+# TODO Figure out why the output of parallel_calls isn't jumbled
+#  up for blocking_listener_primitive or blocking_listener_spawns_threads.
+
+sequential_calls() {
+    for i in {1..5}; do
+        curl localhost:3000/$i 2>/dev/null
+    done
+}
+
+parallel_calls() {
+    for i in {1..5}; do
+        curl localhost:3000/$i 2>/dev/null &
+    done
+}
+
+echo SEQUENTIAL CALLS
+echo $(sequential_calls)
+
+echo -e "\n\nPARALLEL CALLS"
+echo $(parallel_calls)
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::cmp::min;
 use std::io::{Read, Result, Write};
 use std::net::{TcpListener, TcpStream};
 
-
 fn handle_connection(mut connection: TcpStream) -> Result<()> {
     let mut bytes_read = 0;
     let mut request = [0u8; 1024];
@@ -12,7 +11,7 @@ fn handle_connection(mut connection: TcpStream) -> Result<()> {
     loop {
         let num_bytes = Read::by_ref(&mut connection).take(5).read(&mut request[bytes_read..])?;
         // let num_bytes = connection.read(&mut request[bytes_read..])?;
-        println!("read {num_bytes} bytes");
+        // println!("DEBUG read {num_bytes} bytes");
         bytes_read += num_bytes;
 
         if num_bytes == 0 {
@@ -26,15 +25,17 @@ fn handle_connection(mut connection: TcpStream) -> Result<()> {
     }
 
     let request = String::from_utf8_lossy(&request[..bytes_read]);
-    println!("finished reading request: {request}");
+    // TODO Use an actual logging library
+    println!("DEBUG finished reading request: {request}");
 
+    // TODO Base the response off the input provided
     // Write response
     println!("writing to connection");
     let response = concat!(
         "HTTP/1.1 200 OK\r\n",
         "Content-Length: 12\n",
         "Connection: close\r\n\r\n",
-        "Hello world!"
+        "COMPLETED!\n\n"
     ).as_bytes();
     let response_bytes = response.len();
 
@@ -44,7 +45,7 @@ fn handle_connection(mut connection: TcpStream) -> Result<()> {
         let num_bytes = connection.write(&response[bytes_written..min(bytes_written + 5, response_bytes)])?;
         // let num_bytes = connection.write(&response[bytes_written..])?;
         bytes_written += num_bytes;
-        println!("wrote {num_bytes} bytes, {bytes_written}/{response_bytes}");
+        // println!("DEBUG wrote {num_bytes} bytes, {bytes_written}/{response_bytes}");
 
         if num_bytes == 0 {
             println!("client disconnected unexpectedly while writing response");
@@ -61,14 +62,35 @@ fn handle_connection(mut connection: TcpStream) -> Result<()> {
     connection.flush()
 }
 
-fn main() {
-    // Blocking TcpListener
+fn blocking_listener_primitive() {
     let blocking_listener = TcpListener::bind("localhost:3000").unwrap();
-    println!("waiting to accept connection");
-    let (connection, socket_addr) = blocking_listener.accept().unwrap();
-    println!("accepted connection to {socket_addr}");
 
-    if let Err(e) = handle_connection(connection) {
-        println!("failed to handle connection {e}")
+    loop {
+        // Primitive blocking TcpListener
+        println!("about to wait to accept connection");
+        let (connection, _) = blocking_listener.accept().unwrap();
+        println!("accepted connection");
+
+        if let Err(e) = handle_connection(connection) {
+            println!("failed to handle connection {e}")
+        }
     }
+}
+
+fn blocking_listener_spawns_threads() {
+    let blocking_listener = TcpListener::bind("localhost:3000").unwrap();
+
+    loop {
+        // Spawning threads for each connection
+        println!("about to wait to accept connection");
+        let (connection, _) = blocking_listener.accept().unwrap();
+        println!("accepted connection");
+
+        std::thread::spawn(|| handle_connection(connection));
+    }
+}
+
+fn main() {
+    blocking_listener_primitive();
+    // blocking_listener_spawns_threads();
 }
